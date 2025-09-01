3b02be2ee739ef81c86ad2edc202284409692fc1
Yash Raj <kyashraj991@gmail.com>
2025-09-01 21:19:04 +0530

    feat: refactor resume data handling in file upload API for improved error management and session validation

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,23 +1,28 @@
+import { auth } from '@/auth';
+import { withApiHandler } from '@/lib/apiHandler';
 import { generateSummary } from '@/lib/gemini';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
-export async function POST(req: NextRequest) {
-  try {
-    const formData = await req.formData();
-    const file = formData.get('file') as File;
+const getResumeData = async (req: NextRequest) => {
+  const session = await auth();
 
-    if (!file) {
-      return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
-    }
-
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-
-    const summary = await generateSummary(file);
-
-    return NextResponse.json({ summary: summary }, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Upload failed' }, { status: 500 });
+  if (!session?.user?.id) {
+    throw new Error('Unauthorized');
   }
-}
+
+  const formData = await req.formData();
+  const file = formData.get('file') as File;
+
+  if (!file) {
+    throw new Error('File not found!');
+  }
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  const summary = await generateSummary(file);
+
+  return summary;
+};
+
+export const POST = withApiHandler(getResumeData);
