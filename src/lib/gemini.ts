cc9480a63fab9c3f8fe0dd507b11592c2ffe6e15
Yash Raj <kyashraj991@gmail.com>
2025-09-01 21:18:52 +0530

    feat: enhance resume summary generation with detailed JSON structure and improved parsing logic

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -4,24 +4,60 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function generateSummary(file: File) {
   const prompt = `
-    Extract the following details from the resume:
-    - Full Name
-    - Email
-    - Phone Number
-    - Skills (as a list)
-    - Education (with degree + year)
-    - Work Experience (company, role, duration, description)
-  `;
+You are a resume parser. 
+Extract the following details from the PDF and return them in valid JSON only.
+Follow this template exactly:
+
+{
+  "fullName": "John Doe",
+  "email": "john@example.com",
+  "phoneNumber": "+1-123-456-7890",
+  "skills": ["JavaScript", "React"],
+  "education": [
+  {
+  
+    "type": "university",            
+    "degree": "B.Sc Computer Science", 
+    "field": "Computer Science",     
+    "institution": "University of Delhi",
+    "startYear": 2020,              
+    "endYear": 2023,
+    "grade": "8.5 CGPA"              
+   }, 
+ ],
+  
+  "workExperience": [
+    { "company": "TechCorp", "role": "Engineer", "duration": "2022-2023", "description": "Worked on MERN apps" }
+  ], 
+  "Projects": ["Portfolio Website"],
+  "aboutResume": [
+    {
+      "summary": "Resume overview here",
+      "achievements": "Awards etc.",
+      "improvements": "Suggestions",
+      "pros": "Strengths",
+      "cons": "Weaknesses",
+      "rating": 8
+    }
+  ]
+}
+
+Important:
+- Do not confuse projects with work experience.
+- Rating out of 10.
+- Respond with JSON only.
+- Social Media handles should be in url format
+`;
 
   const bytes = await file.arrayBuffer();
   const base64 = Buffer.from(bytes).toString('base64');
 
   const contents = [
-    // { text: 'Summarize this document' },
+    { text: prompt },
     {
       inlineData: {
         mimeType: 'application/pdf',
-        data: base64
+        data: base64,
         // data: Buffer.from(pdfResp).toString('base64'),
       },
     },
@@ -34,7 +70,13 @@ export async function generateSummary(file: File) {
       systemInstruction: prompt,
     },
   });
-  
-  return response.text
 
+  const raw = response.text ?? '';
+
+  const match = raw.match(/\{[\s\S]*\}/);
+
+  if (!match) throw new Error('No JSON found in response');
+
+  const summary = JSON.parse(match[0]);
+  return summary;
 }
